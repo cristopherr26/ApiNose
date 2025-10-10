bc1c89e4a75fcd088b39645ad959fa531df553f0
Cristopher Ospina <180292639+cristopherr26@users.noreply.github.com>
2025-10-10 13:04:59 -0500

    Creación de la validación transactionIsStarted (Tarea sesión 12)

--- ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/SqlConnection.java
+++ ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/SqlConnection.java
@@ -1,12 +1,9 @@
 package co.edu.uco.nose.data.dao.entity;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 
 
-import co.edu.uco.nose.crosscuting.exception.NoseException;
-import co.edu.uco.nose.crosscuting.helper.ObjectHelper;
-import co.edu.uco.nose.crosscuting.messagescatalog.MessagesEnum;
+import co.edu.uco.nose.crosscuting.helper.SqlConnectionHelper;
 
 public abstract class SqlConnection {
 	
@@ -23,32 +20,11 @@ public abstract class SqlConnection {
 	}
 
 	private void setConnection(Connection connection) {
-		if(ObjectHelper.isNull(connection)) {
-			var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_IS_EMPTY.getContent();
-			var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_IS_EMPTY.getContent();
-			throw NoseException.create(userMessage, technicalMessage);
-		}
 		
-		try {
-			
-			if(connection.isClosed()) {
-				
-				var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_IS_CLOSED.getContent();
-				var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_IS_CLOSED.getContent();
-				throw NoseException.create(userMessage, technicalMessage);
-				
-			}
-			
-		} catch (final SQLException exception) {
-			
-			var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_CONNECTION_STATUS.getContent();
-			var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_CONNECTION_STATUS.getContent();
-			throw NoseException.create(exception, userMessage, technicalMessage);
-
-		}
-		
+		SqlConnectionHelper.ensureConnectionIsOpen(connection);
 		
 		this.connection = connection;
+	
 	}
 	
 	
